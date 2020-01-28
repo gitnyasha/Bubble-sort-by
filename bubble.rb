0ffe65b0ea470cfef75e822965027fad4122bd20
nyasha chikari <marshall.chikari@gmail.com>
2020-01-28 23:38:17 +0200

    TSE changes

--- bubble.rb
+++ bubble.rb
@@ -30,6 +30,4 @@ def bubble_sort_by(array)
   array
 end
 
-puts bubble_sort_by(%w[hi hello hey]) do |left, right|
-  left.length - right.length
-end
+puts bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
