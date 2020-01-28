89d0e1edaec517242ddfb7711d244fab09faf9ba
nyasha chikari <marshall.chikari@gmail.com>
2020-01-28 19:26:56 +0200

    bubble sorted function

---
+++ bubble.rb
@@ -0,0 +1,18 @@
+def bubble_sort_by(array)
+    x = array.length
+    loop do
+        sorted = false
+        (x-1).times do |i|
+            if array[i] < array[i + 1]
+                array[i+1], array[i] = array[i], array[i+1]
+                sorted = true
+            end
+        end
+        break if not sorted
+    end
+    array
+end
+
+puts array = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+ end
