a7b7657b7e38746963c3d8c1fba41b5e33fb99cd
nyasha chikari <marshall.chikari@gmail.com>
2020-01-28 22:01:25 +0200

    made changes flagged by the TSE

--- bubble.rb
+++ bubble.rb
@@ -1,3 +1,20 @@
+def bubble_sort(array)
+    x = array.length
+    loop do
+      sorted = false
+      (x - 1).times do |i|
+        if array[i] > array[i + 1]
+          array[i + 1], array[i] = array[i], array[i + 1]
+          sorted = true
+        end
+      end
+      break unless sorted
+    end
+    array
+  end
+
+  puts array1 = bubble_sort([4,3,78,2,0,2]) 
+
 def bubble_sort_by(array)
   x = array.length
   loop do
